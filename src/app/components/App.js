edd0fa1030e72f65f7f1ef38f6af3b7b3d4a3d9d
Hossein Karami <hi@hosseinkarami.com>
2017-07-17 15:43:42 +0430

    Fix Bug 👀

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -8,9 +8,11 @@ export class App extends React.Component {
         <div className="header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React Starter Kit</h2>
-        </div>
+          <iframe src="https://ghbtns.com/github-btn.html?user=HosseinKarami&repo=React-Starter-Kit&type=star&count=true" frameborder="0" scrolling="0" width="90px" height="20px"></iframe>
+          <iframe src="https://ghbtns.com/github-btn.html?user=HosseinKarami&repo=React-Starter-Kit&type=fork&count=true" frameborder="0" scrolling="0" width="90px" height="20px"></iframe>
+       </div>
         <p className="App-intro">
-          To get started, edit <code>src/components/App.js</code> and save to reload.
+          To get started, Fork from top and then edit <code>src/components/App.js</code> and save to reload.
         </p>
       </div>
     );
